e3c5c8e93ae7abfc1b943a186db0af1c0d052470
crazy0629 <kenzo.egghead@gmail.com>
2023-06-24 18:27:36 -0700

    remove dist file

--- dist/interfaces.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.walletResponse = void 0;
-const walletResponse = (args) => {
-    return args;
-};
-exports.walletResponse = walletResponse;
